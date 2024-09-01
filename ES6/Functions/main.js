05437a2894fb674390df144716222315c56af86c
Gagan Bansal <bansalgagan2004@gmail.com>
2024-09-01 16:41:28 +0530

    "Function Added"

--- ES6/Functions/main.js
+++ ES6/Functions/main.js
@@ -4,3 +4,66 @@
 // allowing developers to encapsulate reusable code blocks,
 // enhance code readability, and facilitate modular programming.
 
+
+// That we are passing in function calling : Argument
+// That we recieved from function calling : Parameter 
+
+//  ---------- Simplest Function -------------
+
+// Function Definition
+function Hello(){ // 0 Parameter
+    console.log("Hello, I am simplest Function")
+}
+
+// Function Call
+Hello(); // 0 Argument function
+
+
+
+// function printCount(){
+//     for(let i = 1; i<=10; i++){
+//         console.log(i);
+//     }
+// }
+// printCount();
+
+
+// Parameterized Function
+function printNumber(n){ // 1 Parameter
+    console.log("Printing Number : ", n);
+}
+printNumber(5); // 1 Argument 
+
+// we can pass multiple argument
+
+
+// ----------------- Return Function ------------------
+
+function getSum(a, b){
+    let sum = a + b;
+    return sum;
+}
+
+console.log("Sum of 4 + 5 : ", getSum(4, 5));
+
+
+// -------------- Function Expression ------------------
+
+let getMultiplication = function(a, b){
+    return a*b;
+}
+let res = getMultiplication(10, 20);
+console.log(res);
+
+
+// ------------------ Arrow Function -----------------
+
+let getExp = (x, y) => {
+    return x**y;
+}
+console.log(getExp(2, 5));
+
+
+// -------------------- Reference -----------------------
+// read : https://www.w3schools.com/js/js_functions.asp
+// video : https://www.youtube.com/watch?v=nqC-UlGTssg&list=PLDzeHZWIZsTo0wSBcg4-NMIbC0L8evLrD&index=43
--- readme.md
+++ readme.md
@@ -1,10 +1,17 @@
 <h1>JavaScript Core</h1>
 
+<p> Topics that we are covering </p>
+
+<hr>
 <h4>ES6 features</h4>
+<hr>
 <p> > Hoisting</p>
 <p> > Variables and Constants</p>
 <p> > Functions</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
+<p> > Loops </p>
+<p> > Spread and Rest Operators</p>
+<p> > Sets and Maps</p>
+<p> > Destructing</p>
+<p> > Web Storage</p>
+<p> > Arrow Function</p>
+<p> > Function/Method Chaining</p>
