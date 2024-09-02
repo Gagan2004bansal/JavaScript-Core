5e702a1d41279a013f7983fad1f25d0bf0227865
Gagan Bansal <bansalgagan2004@gmail.com>
2024-09-02 12:39:27 +0530

    Variables and Constants Added

---
+++ ES6/VariablesandConstants/main.js
@@ -0,0 +1,22 @@
+// Variables and Constants
+
+// var, let and const
+
+
+// It can be updated and re-declared in the same scope.
+// It can be declared without initialization.
+// These variables are hoisted.
+var name1 = "Gagan Bansal";
+console.log(name1);
+
+
+// It can be updated but cannot be re-declared in the same scope.
+// It can be declared without initialization.
+let University = "Chitkara University";
+console.log(University);
+
+
+// It can neither be updated or re-declared in any scope.
+// It cannot be declared without initialization.
+const stream1 = "Btech";
+console.log(stream1);
